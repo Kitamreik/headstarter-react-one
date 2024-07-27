c2c0f6db1a3511f510a23894b8da81820cce804b
kitamreik <kitdamreik@gmail.com>
2024-07-27 05:04:16 -0600

    minor notes for fetch command

--- headstarter-react-one/src/App.js
+++ headstarter-react-one/src/App.js
@@ -5,7 +5,15 @@ function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/data')
+    //Urls that can be tested:
+    /*
+    http://localhost:5000/
+    http://localhost:5000/about
+    http://localhost:5000/error
+    http://localhost:5000/api/data
+    http://localhost:5000/api/data/create
+    */
+    fetch('http://localhost:5000/')
       .then(response => response.json())
       .then(data => setData(data.message))
       .catch(error => console.error('Error fetching data:', error));
@@ -13,7 +21,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>{data ? data : 'Loading...'}</h1>
+      <h1>{data ? data : 'Loading...'}</h1>     
     </div>
   );
 }
